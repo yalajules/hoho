9780be287204234cac122cb3e45d45fee9ba5e17
yalajules <yalajules@yahoo.fr>
2018-04-06 12:07:31 +0200

    encodage

--- bataillenavalesimple.py
+++ bataillenavalesimple.py
@@ -1,10 +1,11 @@
-a = 7
+# coding : utf8
+a = 42
 b = 4
 print ("A vous de jouer.")
 x = int(input())
 y = int(input())
 if x == a and y == b :
-    print("Coulé")
+    print("CoulÃ©")
 else:
     if x == a or y == b :
         print ("en vue")
